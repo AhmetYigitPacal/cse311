80ef9a59a1670b6fa57ca10e854ec8802841d769
ahmetyigit <ahmetyigitpacal@gmail.com>
2025-05-08 23:06:55 +0300

    Added time.sh

--- main.c
+++ main.c
@@ -4,16 +4,11 @@
 #include "matrix.h"
 #include "matrixmult.h"
 
-#define RUN_COUNT 1
-
 int main(int argc, char** argv) {
 	if(argc < 3) {
 		printf("Not enough arguments!\n");
 		return 1;
 	}
-
-	struct timespec start, end;
-	double t;
 
   int algorithm = atoi(argv[1]);
 	int row = atoi(argv[2]), col = atoi(argv[2]);
@@ -21,31 +16,22 @@ int main(int argc, char** argv) {
 	Matrix mat1 = createRandomMatrix(row, col, 103792742);
 	Matrix mat2 = createRandomMatrix(row, col, 932047248);
 
-	for(int i = 0; i < RUN_COUNT; i++) {
-    printf("Multiplying Matrices...\n");
-		timespec_get(&start, TIME_UTC);
+  switch(algorithm) {
+    case 0: 
+      freeMatrix(bfMult(mat1, mat2));
+      break;
 
-    switch(algorithm) {
-      case 0: 
-        freeMatrix(bfMult(mat1, mat2));
-        break;
+    case 1:
+      freeMatrix(dncMult(mat1, mat2));
+      break;
 
-      case 1:
-        freeMatrix(dncMult(mat1, mat2));
-        break;
+    case 2:
+      freeMatrix(strassensMult(mat1, mat2));
+      break;
 
-      case 2:
-        freeMatrix(strassensMult(mat1, mat2));
-        break;
-
-      default: 
-        printf("Invalid algorithm choice");
-        break;
-    }
-
-	  timespec_get(&end, TIME_UTC);
-  	t = end.tv_sec - start.tv_sec + (end.tv_nsec - start.tv_nsec) * 1e-9; 
-  	printf("Time it took: %f s\n", t);		
+    default: 
+      printf("Invalid algorithm choice");
+      break;
   }
 
   freeMatrix(mat1);
@@ -53,5 +39,4 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
 
